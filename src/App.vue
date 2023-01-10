9c4635bd8054e3281ae3b0f0b4c8467942fdd02a
soporior <zeref13@live.com>
2023-01-10 10:33:23 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import CesiumPage from './components/CesiumPage.vue'
+</script>
+
+<template>
+  <CesiumPage />
+</template>
+
+<style>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+.cesiumContainer{
+  width:100%;
+}
+</style>
---
+++ src/components/CesiumPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div ref="cesiumContainer" class="cesiumContainer" id="cesiumContainer"></div>
+</template>
+<script lang="ts" setup>
+import { defineComponent, ref, onMounted } from 'vue'
+
+// defineProps({
+//   msg: {
+//     type: String,
+//     required: true,
+//   },
+// })
+onMounted(() => {
+  const viewer = new Cesium.Viewer('cesiumContainer')
+})
+const count = ref(0)
+</script>
+<style scoped></style>
